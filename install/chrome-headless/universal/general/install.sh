ce2dd6c78df9b9f8fb10db3df4cecda108f05c66
Toru Nakai <nakai@scalewx.com>
2025-12-14 11:15:53 +0900

    refactor chrome headless installer

--- install/chrome-headless/universal/general/install.sh
+++ install/chrome-headless/universal/general/install.sh
@@ -1,3 +1,13 @@
 # https://developer.chrome.com/blog/chrome-headless-shell?hl=ja
 
-npx @puppeteer/browsers install chrome-headless-shell@stable
+PREFIX=$HOME/.local/share
+EXEC=$HOME/.local/share/chrome-headless-shell/*/chrome-headless-shell-*/chrome-headless-shell
+LINK=$HOME/.local/bin/chrome-headless-shell
+
+rm -rf $PREFIX/chrome-headless-shell $LINK
+mkdir -p $PREFIX $HOME/.local/bin
+
+npx @puppeteer/browsers install chrome-headless-shell@stable --path $PREFIX
+
+ln -s $EXEC $LINK
+echo "Created link $LINK"
